fb0b03e923043ce313e13e2a527866cffdd8db13
Ülla <90785712+ullamonster@users.noreply.github.com>
2022-03-26 01:28:33 +0000

    Update setupMainNode.sh

--- dapp/resources/helpers/setupMainNode.sh
+++ dapp/resources/helpers/setupMainNode.sh
@@ -21,6 +21,7 @@ make
 sudo make install
 
 ### IMPORTANT: Answer NO to installing the haskell-language-server (HLS) and NO to stack; Answer YES to automatically add the required PATH variable
+echo "Answer YES to automatically add the required PATH variable; Answer NO to installing the haskell-language-server (HLS) and NO to stack"
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 source /home/user/.ghcup/env
 
