838b72c06f1ef89c349c645ca620cc40c488e940
Ülla <90785712+ullamonster@users.noreply.github.com>
2022-08-31 00:20:54 +0000

    Update dcostats.js

--- assets/dcostats.js
+++ assets/dcostats.js
@@ -1,6 +1,8 @@
-jQuery('#minted_seats').on('change', function(){
-  myseatget = jQuery(this).val()
-  jQuery('#seat_size').html(myseatget);
-  myshare = (parseFloat(myseatget) / parseFloat('6.906')).toFixed(3).toLocaleString();
-  jQuery('#seat_ownership').html('~' + (parseFloat(myshare) * 100).toFixed(2).toLocaleString() + '%');
+jQuery( document ).ready(function() {
+  jQuery('#minted_seats').on('change', function(){
+    myseatget = jQuery(this).val()
+    jQuery('#seat_size').html(myseatget);
+    myshare = (parseFloat(myseatget) / parseFloat('6.906')).toFixed(3).toLocaleString();
+    jQuery('#seat_ownership').html('~' + (parseFloat(myshare) * 100).toFixed(2).toLocaleString() + '%');
+  });
 });
